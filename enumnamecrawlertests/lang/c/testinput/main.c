082f0cd30f907b951ad2101ae51eeb36518c265e
Yinyin L <yinyinl@gmail.com>
2018-03-11 00:40:35 +0800

    add test input code file

---
+++ enumnamecrawlertests/lang/c/testinput/main.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+#include "errorcode.h"
+
+int positive_divide(int dividend, int divisor) {
+	if (dividend < 0) {
+		return TESTINPUT_DIVIDEND_NEGATIVE;
+	}
+	if (divisor < 0) {
+		return TESTINPUT_DIVISOR_NEGATIVE;
+	}
+	if (divisor == 0) {
+		return TESTINPUT_DIVIDE_BY_ZERO;
+	}
+	return (dividend / divisor);
+}
+
+int main(int argc, char *argv[]) {
+	int quotient;
+	quotient = positive_divide(-10, 3);
+	if (quotient < 0) {
+		printf("ERR: %s.\n", errorcode_string(quotient));
+	} else {
+		printf("%d = 10 / 3\n", quotient);
+	}
+	return 0;
+}
